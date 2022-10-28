227c421122874d3ecace51897c296354c9df3ce7
Rick <53375860+modularsamples@users.noreply.github.com>
2022-10-28 22:41:32 +0100

    move

--- Quetzalcoatl/Scripts/ScriptProcessors/vecfm/Interface.js
+++ Quetzalcoatl/Scripts/ScriptProcessors/vecfm/Interface.js
@@ -1,5 +1,35 @@
 Content.makeFrontInterface(888, 740);
 
+//presets
+const var Pbox = [];
+
+Pbox[0] = Content.getComponent("settings");
+Pbox[1] = Content.getComponent("blank1");
+Pbox[2] = Content.getComponent("blank2");
+
+const var pre= Content.getComponent("presets");
+
+inline function onpresetsControl(component, value)
+{
+    for (i = 0; i < Pbox.length; i++)
+        Pbox[i].showControl(value - 1 == i);
+};
+
+
+Content.getComponent("presets").setControlCallback(onpresetsControl);
+
+function onControl(number, value)
+{
+    if(number == widget1)
+    {
+        Module1.setAttribute(Module1.Parameter, value);
+    }
+    else if (number == widget2)
+    {
+        Module2.setAttribute(Module2.Parameter, value);
+    }
+}
+
 const var Sampler1 = Synth.getSampler("Sampler1");
 const var Sampler2 = Synth.getSampler("Sampler2");
 const var Sampler3 = Synth.getSampler("Sampler3");
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Quetzalcoatl
- 
+
+Morphing Rompler thing with FM and serpent gods. 
+
 ![Quetzalcoatl](https://raw.githubusercontent.com/publicsamples/Quetzalcoatl/main/q.png)
 
   
