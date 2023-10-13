660f64fa6489455ec438606ef6c10ef649c4fc78
Rick <53375860+RickTaylor79@users.noreply.github.com>
2023-10-13 14:49:32 +0100

    xml

--- Quetzalcoatl/Scripts/ScriptProcessors/Quetzalcoatl/Interface.js
+++ Quetzalcoatl/Scripts/ScriptProcessors/Quetzalcoatl/Interface.js
@@ -7,8 +7,38 @@ Synth.setUseUniformVoiceHandler("Quetzalcoatl", true);
 include("ExpansionWaves.js");
 include("waves.js");
 
+//Draw Combobox
+
+const var laf = Engine.createGlobalScriptLookAndFeel();
+laf.registerFunction("drawComboBox", function(g, obj)
+{
+    g.setColour(obj.bgColour);
+    g.drawRoundedRectangle(obj.area, 3.0, 3.0);
+    g.setColour(Colours.withAlpha(obj.textColour, (obj.enabled && obj.active) ? 1.0 : 0.2));
+ //  g.setFont("Montserrat", 16.0);
+   
+  
+    var a = obj.area;
+    g.drawAlignedText(obj.text, [a[0] + 0, a[0], a[2]-1, a[3]], "centred");
 
 
+
+});
+
+
+
+
+const var gatemods = Content.getComponent("gatemods");
+
+const var gate = Content.getComponent("gate");
+
+
+inline function ongatemodsControl(component, value)
+{
+gate.showControl(value);        gate.showControl(1-value);
+};
+
+Content.getComponent("gatemods").setControlCallback(ongatemodsControl);
 function onNoteOn()
 {
 	
